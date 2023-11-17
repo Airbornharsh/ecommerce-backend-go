99ef4c44ef4552de85f7242ebdbd8146b777126b
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-17 12:08:52 +0530

    Added Get USer Handler

--- pkg/handlers/user.go
+++ pkg/handlers/user.go
@@ -1,9 +1,27 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/airbornharsh/ecommerce-backend-go/pkg/models"
+	"github.com/gin-gonic/gin"
+)
 
-func GetUserHandler(c *gin.Context){
+func GetUserHandler(c *gin.Context) {
+	tempUser, exists := c.Get("user")
+
+	if !exists {
+		c.JSON(401, gin.H{
+			"message": "Unauthorized",
+		})
+		return
+	}
+
+	user := tempUser.(models.User)
+
+	c.JSON(200, gin.H{
+		"message": "User Found",
+		"user":    user,
+	})
 }
 
-func UpdateUserHandler(c *gin.Context){
+func UpdateUserHandler(c *gin.Context) {
 }
