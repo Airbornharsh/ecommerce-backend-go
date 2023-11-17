5f99fa5e799d2ac80430eb94f39ea9bff512856f
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-17 12:02:30 +0530

    Added Auth MiddleWare for Admin User

--- pkg/middleware/authMiddleware.go
+++ pkg/middleware/authMiddleware.go
@@ -59,3 +59,62 @@ func UserTokenVerifyMiddleWare(c *gin.Context) {
 	c.Set("user", user)
 	c.Next()
 }
+
+func AdminTokenVerifyMiddleWare(c *gin.Context) {
+	Auth := c.Request.Header.Get("Authorization")
+
+	if Auth == "" {
+		c.JSON(401, gin.H{
+			"message": "Unauthorized",
+		})
+		c.Set("user", nil)
+
+		c.Abort()
+		return
+	}
+
+	tempUser, err := helpers.GetClaims(c, Auth)
+	if helpers.ErrorResponse(c, err, 401) {
+		c.Abort()
+		return
+	}
+
+	var user models.User
+
+	q := `SELECT user_id, username, email, phone_number, role FROM users WHERE user_id = ` + strconv.Itoa(int(tempUser.UserID)) + `;`
+
+	rows, err := database.DB.Query(q)
+	if helpers.ErrorResponse(c, err, 500) {
+		c.Abort()
+		return
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&user.UserID, &user.Username, &user.Email, &user.PhoneNumber, &user.Role)
+		if helpers.ErrorResponse(c, err, 500) {
+			c.Abort()
+			return
+		}
+	}
+
+	if user.UserID == 0 {
+		c.JSON(401, gin.H{
+			"message": "Unauthorized",
+		})
+		c.Set("user", nil)
+
+		c.Abort()
+		return
+	}
+
+	if user.Role != "admin" {
+		c.JSON(401, gin.H{
+			"message": "Unauthorized",
+		})
+		c.Set("user", nil)
+		c.Abort()
+	}
+
+	c.Set("user", user)
+	c.Next()
+}
