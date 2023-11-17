f5650f5a49169a222765a82b01b8efc83f5ccc38
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-17 22:28:47 +0530

    Added Address Routes and Handler's skeleton

---
+++ pkg/handlers/addressHandler.go
@@ -0,0 +1,23 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func GetAllAddressHandler(c *gin.Context) {
+
+}
+
+func GetAddressHandler(c *gin.Context) {
+
+}
+
+func CreateAddressHandler(c *gin.Context) {
+
+}
+
+func UpdateAddressHandler(c *gin.Context) {
+
+}
+
+func DeleteAddressHandler(c *gin.Context) {
+
+}
---
+++ pkg/routes/aaddress.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/airbornharsh/ecommerce-backend-go/pkg/handlers"
+	"github.com/airbornharsh/ecommerce-backend-go/pkg/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func AddressInit(api *gin.RouterGroup) {
+	user := api.Group("/user")
+	address := user.Group("/address")
+
+	address.GET("/", middlewares.UserTokenVerifyMiddleWare, handlers.GetAllAddressHandler)
+	address.GET("/:id", middlewares.UserTokenVerifyMiddleWare, handlers.GetAddressHandler)
+	address.POST("/", middlewares.UserTokenVerifyMiddleWare, handlers.CreateAddressHandler)
+	address.PUT("/:id", middlewares.UserTokenVerifyMiddleWare, handlers.UpdateAddressHandler)
+	address.DELETE("/:id", middlewares.UserTokenVerifyMiddleWare, handlers.DeleteAddressHandler)
+}
