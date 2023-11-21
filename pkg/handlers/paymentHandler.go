35762cc9247b5039b715a30834c88edd9b9d0534
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-11-21 09:46:29 +0530

    Added all routes of payment

--- pkg/handlers/paymentHandler.go
+++ pkg/handlers/paymentHandler.go
@@ -3,5 +3,17 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func CreatePaymentHandler(c *gin.Context) {
+
+}
+
+func FailedPaymentHandler(c *gin.Context) {
+
+}
+
+func SuccessPaymentHandler(c *gin.Context) {
+
+}
+
+func CancelPaymentHandler(c *gin.Context) {
 	
 }
--- pkg/routes/payment.go
+++ pkg/routes/payment.go
@@ -2,11 +2,16 @@ package routes
 
 import (
 	"github.com/airbornharsh/ecommerce-backend-go/pkg/handlers"
+	"github.com/airbornharsh/ecommerce-backend-go/pkg/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func PaymentInit(user *gin.RouterGroup) {
 	payment := user.Group("/payment")
 
-	payment.POST("/", handlers.CreatePaymentHandler)
+	payment.POST("/", middlewares.UserTokenVerifyMiddleWare, handlers.CreatePaymentHandler)
+	payment.POST("/failed", middlewares.UserTokenVerifyMiddleWare, handlers.FailedPaymentHandler)
+	payment.POST("/success", middlewares.UserTokenVerifyMiddleWare, handlers.SuccessPaymentHandler)
+	payment.POST("/cancel", middlewares.UserTokenVerifyMiddleWare, handlers.CancelPaymentHandler)
+
 }
